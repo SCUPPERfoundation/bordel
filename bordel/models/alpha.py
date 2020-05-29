e7a2a80ed69aa6bd8dea53e2fb17831acd394890
David Van Duzer <dvd@tennica.net>
2020-05-28 21:49:16 -0600

    import JSONB type correctly

--- bordel/models/alpha.py
+++ bordel/models/alpha.py
@@ -1,5 +1,7 @@
+from sqlalchemy.dialects.postgresql import JSONB
+
 from .. import db
 
 class ReviewDoc(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    blob = db.Column(db.JSONB)
+    blob = db.Column(JSONB)
