39bdf24c35002381bbe85b6bf25dddc992126d35
David Van Duzer <dvd@tennica.net>
2020-05-28 21:39:34 -0600

    postgres has JSON now, huh

--- bordel/models/alpha.py
+++ bordel/models/alpha.py
@@ -1,8 +1,5 @@
-from sqlalchemy.dialects.postgresql import HSTORE
-from sqlalchemy.ext.mutable import MutableDict
-
 from .. import db
 
 class ReviewDoc(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    blob = db.Column(MutableDict.as_mutable(HSTORE))
+    blob = db.Column(db.JSON)
