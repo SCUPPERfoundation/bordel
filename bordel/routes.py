1f8dbdd17d9dd07f532de3347e2102c8aa5ef0d0
David Van Duzer <dvd@tennica.net>
2020-05-28 20:48:15 -0600

    parameters to templates must be keyword arguments

--- bordel/routes.py
+++ bordel/routes.py
@@ -7,5 +7,5 @@ from .models.alpha import ReviewDoc
 def index():
     page = 1 # shim. fully deal with pagination later
     blobs = ReviewDoc.query.paginate(page)
-    return render_template('home.html', blobs)
+    return render_template('home.html', blobs=blobs)
 
